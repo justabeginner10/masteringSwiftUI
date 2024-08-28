5bd6bcfae294df74e344834ec5b7484462874dac
Aditya <aditya@Varshas-MBP.lan>
2024-08-28 10:53:15 +0530

    FIRST: First commit added

---
+++ MasteringSwiftUI/MasteringSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringSwiftUIApp.swift
+//  MasteringSwiftUI
+//
+//  Created by Aditya on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MasteringSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
